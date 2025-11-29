ec9be4f8b79f07819594a7c3288b32fea1809a47
rekliure <34690056+rekliure@users.noreply.github.com>
2025-11-29 07:57:41 +0530

    Created Robot txt file

--- app/robots.ts
+++ app/robots.ts
@@ -4,11 +4,20 @@ export default function robots(): MetadataRoute.Robots {
   const site = (process.env.NEXT_PUBLIC_SITE_URL || "https://durlabhclapfoundation.org").replace(/\/+$/, "");
 
   return {
+
+
+
     rules: {
       userAgent: "*",
       allow: "/",
       disallow: ["/api/", "/_next/"],
+
+
+
+
     },
     sitemap: `${site}/sitemap.xml`,
   };
 }
+
+
